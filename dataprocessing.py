4eeb4209f1e3672f83c078039c103f288fa9bc81
Beloslava <beloslava.malakova@gmail.com>
2022-03-12 10:12:21 +0200

    prediction accuracy of logistic regression

--- dataprocessing.py
+++ dataprocessing.py
@@ -1,3 +1,4 @@
 import pandas as pd
 
 test = pd.read_json("../data/test")
+train = pd.read_json("../data/train")
--- models/mobilev2.py
+++ models/mobilev2.py
@@ -4,27 +4,29 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt 
 from sklearn.metrics import accuracy_score
-from dataprocessing.py import test
+from dataprocessing.py import test, train
 from sklearn import linear_model
+from sklearn.model_selection import train_test_split
 
-test.info()
-test.head()
 
-sns.catplot(x = 'perimeter_mean', y = 'type rocket', data = test, order=['1 cluster', '0 grad'])
-test.head()
+train.info()
+train.head()
 
-X,y = test[['rocket']], test[['type']]
+sns.catplot(x = 'perimeter_mean', y = 'type rocket', data = train, order=['1 cluster', '0 grad'])
+train.head()
+
+X,y = train[['rocket']], train[['type']]
 
 model = linear_model.LinearRegression()
 model.fit(X, y)
 preds = model.predict(X)
 
-sns.scatterplot(x='rocket', y='type', data=test)
+sns.scatterplot(x='rocket', y='type', data=train)
 plt.plot(X, preds, color='r')
 plt.legend(['Linear Regression Fit', 'Data'])
 
 boundary = 15 
-sns.catplot(x = 'rocket', y = 'type', data = test, order=['1 (cluster)', '0 (grad)'])
+sns.catplot(x = 'rocket', y = 'type', data = train, order=['1 (cluster)', '0 (grad)'])
 plt.plot([boundary, boundary], [-.2, 1.2], 'g', linewidth = 2)
 
 def boundary_classifier(target_boundary, radius_mean_series):
@@ -38,21 +40,21 @@ def boundary_classifier(target_boundary, radius_mean_series):
   return result
 
 chosen_boundary=10
-y_pred=boundary_classifier(chosen_boundary, test['perimeter_mean'])
-test['predicted']=y_pred
-y_true=test['type']
-sns.catplot(x='perimeter_mean', y='type', hue='predicted', data=test, order=['1 (cluster)', '0(grad)'])
+y_pred=boundary_classifier(chosen_boundary, train['perimeter_mean'])
+train['predicted']=y_pred
+y_true=train['type']
+sns.catplot(x='perimeter_mean', y='type', hue='predicted', data=train, order=['1 (cluster)', '0(grad)'])
 plt.plot([chosen_boundary, chosen_boundary], [-.2, 1.2], 'g', linewidth = 2)
 plt.show()
 
 chosen_boundary = 100
 
-y_pred = boundary_classifier(chosen_boundary, test['perimeter_mean'])
-test['predicted'] = y_pred
+y_pred = boundary_classifier(chosen_boundary, train['perimeter_mean'])
+train['predicted'] = y_pred
 
-y_true = test['type']
+y_true = train['type']
 
-sns.catplot(x = 'perimeter_mean', y = 'diagnosis_cat', hue = 'predicted', data = test, order=['1 (cluster)', '0 (grad)'])
+sns.catplot(x = 'perimeter_mean', y = 'diagnosis_cat', hue = 'predicted', data = train, order=['1 (cluster)', '0 (grad)'])
 plt.plot([chosen_boundary, chosen_boundary], [-.2, 1.2], 'g', linewidth = 2)
 plt.show()
 
@@ -61,4 +63,33 @@ print (y_pred)
 
 accuracy = accuracy_score(y_true,y_pred)
 print(accuracy)
+
+
+X = ['']
+y = ''
+
+X_train = train[X]
+print('X_train, our input variables:')
+print(X_train.head())
+print()
+
+y_train = train[y]
+print('y_train, our output variable:')
+print(y_train.head())
+
+logreg_model = linear_model.LogisticRegression()
+logreg_model.fit(X_train, y_train)
+
+X_test = test[X]
+y_test = test[y]
+
+y_pred = logreg_model.predict(X_test)
+
+test['predicted'] = y_pred.squeeze()
+sns.catplot(x = X[0], y = '', hue = 'predicted', data=test, order=['1', '0'])
+
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
+
+
 
