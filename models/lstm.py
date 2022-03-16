80e73d62be9a67989521526b6d86aafa5a70fc96
Beloslava <beloslava.malakova@gmail.com>
2022-03-16 17:48:31 +0200

    lstm

---
+++ models/lstm.py
@@ -0,0 +1,186 @@
+from collections import Counter
+from importlib.machinery import SourceFileLoader
+import numpy as np
+from os.path import join
+import warnings
+warnings.filterwarnings("ignore")
+from torchtext.vocab import GloVe
+import nltk
+nltk.download('punkt')
+import seaborn as sns
+import matplotlib
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.porter import *
+from nltk.corpus import stopwords
+nltk.download('stopwords' ,quiet=True)
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.metrics import multilabel_confusion_matrix
+from sklearn import metrics
+import gdown
+import string
+import numpy
+import matplotlib.pyplot as plt
+
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from sklearn.model_selection import train_test_split
+from keras.utils.np_utils import to_categorical
+from keras.callbacks import EarlyStopping
+from keras.layers import Dropout
+import re
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+STOPWORDS = set(stopwords.words('english'))
+from bs4 import BeautifulSoup
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import string
+
+
+def get_finance_train():
+  df_train = pd.read_csv("finance_train.csv")
+  return df_train
+def get_finance_test():
+  df_test = pd.read_csv("finance_test.csv")
+  return df_test
+
+PUNCTUATION = '!#$%&()*,-./:;<=>?@^_`{|}~'
+
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
+BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
+STOPWORDS = set(stopwords.words('english'))
+
+def clean_text(text):
+    """
+        text: a string
+        return: modified initial string
+    """
+    text = text.lower() 
+    text = REPLACE_BY_SPACE_RE.sub(' ', text)
+    text = BAD_SYMBOLS_RE.sub('', text)  
+    text = text.replace('x', '')
+    text = ' '.join(word for word in text.split() if word not in STOPWORDS) 
+    return text
+
+def pad_sequences_train(df_train, df_test):
+  tokenizer = Tokenizer(num_words=MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
+  tokenizer.fit_on_texts(df_train['Sentence'].values)
+  word_index = tokenizer.word_index
+  X = tokenizer.texts_to_sequences(df_train['Sentence'].values)
+  X_train = pad_sequences(X, maxlen=MAX_SEQUENCE_LENGTH)
+  return X_train
+
+def pad_sequences_test(df_train, df_test):
+  tokenizer = Tokenizer(num_words=MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
+  tokenizer.fit_on_texts(df_train['Sentence'].values)
+  word_index = tokenizer.word_index
+  X = tokenizer.texts_to_sequences(df_test['Sentence'].values)
+  X_test = pad_sequences(X, maxlen=MAX_SEQUENCE_LENGTH)
+  return X_test
+
+def run_model(X_train, y_train, X_test, y_test, epochs=5, max_sequence_length=256, max_nb_words=1000, embedding_dim=300):
+  if any(x is None for x in [X_train, y_train, X_test, y_test, epochs, max_sequence_length, max_nb_words, embedding_dim]):
+    print('Replace the None values above with your new values before calling the run_model() function.')
+    return None, None, None
+  model = Sequential()
+  model.add(Embedding(max_nb_words+1, embedding_dim, mask_zero=True, input_length=max_sequence_length))
+  model.add(SpatialDropout1D(0.2))
+  model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+  model.add(Dense(n_labels, activation='softmax'))
+  model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+  print(model.summary())
+  history = model.fit(X_train, 
+                    y_train, 
+                    epochs=epochs, 
+                    batch_size=batch_size,
+                    validation_split=0.1,
+                    callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
+  test_loss, test_accuracy = model.evaluate(X_test, y_test)
+  return model, history, test_accuracy
+
+
+EMBEDDING_DIM = 300
+
+
+nltk.download('wordnet')
+
+gdown.download('https://drive.google.com/uc?id=1q4U2gVY9tWEPdT6W-pdQpKmo152QqWLE', 'finance_train.csv', True)
+gdown.download('https://drive.google.com/uc?id=1nIBqAsItwVEGVayYTgvybz7HeK0asom0', 'finance_test.csv', True)
+
+print ("Train & Test Files are loaded")
+
+n_labels = 3
+label_map = {0 : "negative", 
+             1 : "neutral",
+             2 : "positive"}
+
+model = Sequential()
+model.add(Embedding(MAX_NB_WORDS+1, EMBEDDING_DIM, mask_zero=True, input_length=MAX_SEQUENCE_LENGTH))
+model.add(SpatialDropout1D(0.2))
+model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(n_labels, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+epochs = 8 
+batch_size = 32 
+
+history = model.fit(X_train, 
+                    y_train, 
+                    epochs=epochs, 
+                    batch_size=batch_size,
+                    validation_split=0.1,
+                    callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
+
+test_loss, test_accuracy = model.evaluate(X_test, y_test)
+
+training_loss = history.history["loss"]
+validation_loss = history.history["val_loss"]
+
+fig = plt.figure(figsize=(12,6))
+plt.title('Loss over Time')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+
+
+plt.plot(training_loss, label="train")
+plt.plot(validation_loss, label="validation")
+
+plt.legend()
+plt.show()
+
+training_accuracy = history.history["accuracy"]
+validation_accuracy = history.history["val_accuracy"]
+
+fig = plt.figure(figsize=(12,6))
+plt.title('Accuracy over Time')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.plot(training_accuracy, label="train")
+plt.plot(validation_accuracy, label="validation")
+plt.legend()
+plt.show()
+
+model, history, test_acc = run_model(
+    X_train=X_train,
+    y_train=y_train,
+    X_test=X_test,
+    y_test=y_test,
+    epochs=6,
+    max_sequence_length=256,
+    max_nb_words=1500,
+    embedding_dim=280
+)
+print('New test accuracy is {}'.format(test_acc))
